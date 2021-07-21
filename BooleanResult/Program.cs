cb8e12f458ff8e19faedd3dc73ae9ebfbc810b37
Mahesh Dhikonda <mahesh.dhikonda@hotmail.com>
2021-07-21 22:56:16 +0530

    Add project files.

---
+++ BooleanResult/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BooleanResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value for x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for y");
+            int y = Convert.ToInt32(Console.ReadLine());
+            bool result = x < y;
+
+            Console.WriteLine("x is less than y is "+result);
+
+        }
+    }
+}
